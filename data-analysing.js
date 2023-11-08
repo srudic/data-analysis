c563a32e6740b30f1ec6d0187b7fc504ad4668fd
Sanja Rudic <rudic.sanja@gmail.com>
2023-11-08 17:45:07 +0100

    Fix issue with parsing data and exclude Croatian clients

--- data-analysing.js
+++ data-analysing.js
@@ -7,11 +7,21 @@ const fs = require("fs");
 // Use fs.readFile() method to read the file
 fs.readFile(
   "data/apartments-near-Ciovo.json",
-  //   "Users/Sanja/Desktop/analiza-podataka-apartmani/data/apartments-near-Ciovo.json",
+  //   "Users/Sanja/Desktop/analiza-podataka-apartmani/data/apartments-near-Ciovo.json", --> not working
   "utf8",
   function (err, data) {
     // Display the file content
-    console.log(data);
+    const parsedData = JSON.parse(data);
+
+    console.log(parsedData.length);
+    // console.log(data.map((res) => console.log(res)));
+    for (let i = 0; i < parsedData.length; i++) {
+      if (
+        !parsedData[i].Phone_Number.includes("+385") &&
+        parsedData[i].Phone_Number !== ""
+      )
+        console.log(parsedData[i].Phone_Number);
+    }
   }
 );
 
