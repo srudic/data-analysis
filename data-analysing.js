fd2e6cf7a416bb89d31c021307dd2b3d20f5a7e6
Sanja Rudic <rudic.sanja@gmail.com>
2023-11-23 21:15:40 +0100

    Add function for reading each file

--- data-analysing.js
+++ data-analysing.js
@@ -15,13 +15,32 @@ const readFilesInsideDirectory = async (directoryPath) => {
   return arrayOfFiles;
 };
 
+//// function for reading one file
+const readFile = async (fileName) => {
+  let data = [];
+  try {
+    const filePath = `data/${fileName}`;
+    const fileData = await fs.readFile(filePath, { encoding: "utf8" });
+    data = JSON.parse(fileData);
+  } catch (err) {
+    console.log(err);
+  }
+  return data;
+};
+
 const main = async () => {
   const filesArray = await readFilesInsideDirectory(process.cwd() + "/data");
 
   // read individual files
   // -->> some kind of loop
   //  ---- ---- > send file name to function and just wait for the result of the data inside :)
-  console.log(filesArray);
+
+  filesArray.forEach(async (fileName) => {
+    let data = await readFile(fileName);
+    console.log(data);
+  });
+
+  console.log("Files Array", filesArray);
 };
 
 // start executing the program
