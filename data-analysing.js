661a013eebdf2c958166226c326b80840313987f
Sanja Rudic <rudic.sanja@gmail.com>
2024-01-02 15:57:54 +0100

    add writing excel file

--- data-analysing.js
+++ data-analysing.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs/promises");
+const XLSX = require("xlsx");
 
 //// function for reading all files inside directory
 const readFilesInsideDirectory = async (directoryPath) => {
@@ -28,21 +29,47 @@ const readFile = async (fileName) => {
   return data;
 };
 
+let forignNumbers = 0;
+let nativeNumber = 0;
 //// funcion filter data
 const filterData = async (data) => {
+  let filteredDataArray = [];
   try {
     for (let i = 0; i < data.length; i++) {
-      if (!data[i].Phone_Number.includes("+385") && data[i].Phone_Number !== "")
-        console.log(data[i].Title, data[i].Phone_Number);
+      if (
+        !data[i].Phone_Number.includes("+385") &&
+        data[i].Phone_Number !== ""
+      ) {
+        forignNumbers++;
+        filteredDataArray.push({
+          title: data[i].Title,
+          phoneNu: data[i].Phone_Number,
+        });
+      } else {
+        nativeNumber++;
+      }
     }
   } catch (err) {
     console.log(err);
   }
+  return filteredDataArray;
+};
+
+////function for writing in excel file
+const writeExcelFile = async (data) => {
+  try {
+    const workSheet = XLSX.utils.json_to_sheet(data);
+    const workBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workBook, workSheet, "Sheet 1");
+    XLSX.writeFile(workBook, "dataTable.xlsx");
+  } catch (err) {
+    console.log(err);
+  }
+  return data;
 };
 
 const main = async () => {
   const filesArray = await readFilesInsideDirectory(process.cwd() + "/data");
-  console.log("Files Array", filesArray);
   // read individual files
   // -->> some kind of loop
   //  ---- ---- > send file name to function and just wait for the result of the data inside :)
@@ -50,8 +77,9 @@ const main = async () => {
   filesArray.forEach(async (fileName) => {
     const data = await readFile(fileName);
     const filteredData = await filterData(data);
-    // console.log(filteredData);
+    await writeExcelFile(filteredData);
   });
+  console.log("Done");
 };
 
 // start executing the program
